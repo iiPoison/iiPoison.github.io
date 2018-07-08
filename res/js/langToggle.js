cd3f4f564521aef4facbf93894c4b42de6927a50
Artyom Fedotkin <as.fedotkin@yandex.ru>
2018-07-08 21:44:13 +0300

    небольшие корректировки

--- res/js/langToggle.js
+++ res/js/langToggle.js
@@ -3,29 +3,23 @@ $(document).ready(function () {
   //устанавливаем начальный язык
   $(".ru_language").css("display","none");
   $(".en_language").css("display","inline");
-  $("#enlanguage").css("font-size","1.2rem");
-  $("#rulanguage").css("font-size","1rem");
-  $("#enlanguage").css("text-decoration","underline");
-  $("#rulanguage").css("text-decoration","none");
+  $("#enlanguage").css("font-size","1.2rem").css("text-decoration","underline");
+  $("#rulanguage").css("font-size","1rem").css("text-decoration","none");
 
   //переключениче на русский язык
   $("#rulanguage").on('click', function () {
     $(".en_language").css("display","none");
     $(".ru_language").css("display","inline");
-    $("#rulanguage").css("font-size","1.2rem");
-    $("#enlanguage").css("font-size","1rem");
-    $("#rulanguage").css("text-decoration","underline");
-    $("#enlanguage").css("text-decoration","none");
+    $("#rulanguage").css("font-size","1.2rem").css("text-decoration","underline");
+    $("#enlanguage").css("font-size","1rem").css("text-decoration","none");
   });
 
   //переключение на английский язык
   $("#enlanguage").on('click', function () {
     $(".en_language").css("display","inline");
     $(".ru_language").css("display","none");
-    $("#rulanguage").css("font-size","1rem");
-    $("#enlanguage").css("font-size","1.2rem");
-    $("#enlanguage").css("text-decoration","underline");
-    $("#rulanguage").css("text-decoration","none");
+    $("#rulanguage").css("font-size","1rem").css("text-decoration","none");
+    $("#enlanguage").css("font-size","1.2rem").css("text-decoration","underline");
   });
 
 });
